5a5349d7be0ec472ca8231b0120292e9050ec383
Jinnie <hjk7639@gmail.com>
2023-04-23 23:29:52 +0900

    App custom하기

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import NavBar from '@/components/NavBar';
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1 className="active">Hello</h1>
-      <style jsx>{``}</style>
     </div>
   );
 }
