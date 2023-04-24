85b0888bae0d8633a62b73b1d1033e0ab5a1455a
Jinnie <hjk7639@gmail.com>
2023-04-24 11:31:37 +0900

    redirect, rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = 'be47a3caf6ec6cbe4c6a6afb3e0a2766';
+
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:path*',
+        destination: '/new-blog/:path*',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
 
-const API_KEY = 'be47a3caf6ec6cbe4c6a6afb3e0a2766';
-
 export default function Home() {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
     (async () => {
-      const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
